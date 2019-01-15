3f2bb3798cd5a3f19ee223a17a93f29e4a07a178
saiks24 <saiks1337@gmail.com>
2019-01-15 19:51:46 +0300

    Remove old code

--- bin/backup.php
+++ bin/backup.php
@@ -1,10 +1,6 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . '../vendor/autoload.php';
 $app = \saiks24\App\Application::getInstance();
-//var_dump($app->getUploadType());
-//var_dump($app->getDownloadType());
-//var_dump($app->getCreateType());
-//var_dump($app->getConfig());
 echo 'Start create backup....' . PHP_EOL;
 switch ($app->getCreateType()) {
     case 'ssh_exec':
@@ -28,42 +24,3 @@ switch ($app->getUploadType()) {
         break;
 }
 echo 'End!' . PHP_EOL;
-//$config = json_decode(
-//    file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'configs/ssh.json'),
-//    true
-//);
-//if(empty($config)) {
-//    echo 'Wrong config' . PHP_EOL;
-//    exit(-1);
-//}
-//var_dump($config);
-//// TODO Конфиги в отдельную сущность
-//$backupDownLoad = new \saiks24\Downloader\BackupDownloader();
-//$strategy = new \saiks24\Downloader\Strategy\SSHDownloadStrategy(
-//    $config['address'],
-//    $config['port'],
-//    $config['login'],
-//    $config['password'],
-//    $config['filePath']
-//);
-//try {
-//    // Создаем бекап
-////    \saiks24\CreateBackup\BackUpCreator::createBackUp(new \saiks24\CreateBackup\SSHBackup(
-////        $config['address'],
-////        $config['port'],
-////        '/home/u160569/delosystems.ru/www/land/',
-////        '/home/u160569/delosystems.ru/www/files.tar.bz2',
-////        $config['login'],
-////        $config['password']
-////    ));
-//    // Скачиваем
-////    $resultFile = $backupDownLoad->backUpDownlo   ad('./result',$strategy);
-////    var_dump($resultFile);
-//    // Грузим куда нибудь
-//    \saiks24\Uploader\BackupUpload::uploadBackup(new \saiks24\Uploader\Strategy\YandexDiskUploadStrategy());
-//
-//} catch (Exception $e) {
-//    echo get_class($e) . PHP_EOL;
-//    echo $e->getMessage() . PHP_EOL;
-//    exit(-1);
-//}
